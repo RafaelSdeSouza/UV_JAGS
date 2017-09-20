66d3d160cd52eaf02e030c62b08915a4d928807e
Rafael S. de Souza <rafael.2706@gmail.com>
2017-09-20 19:05:36 -0400

    j

--- Logit.R
+++ Logit.R
@@ -10,15 +10,13 @@
 
 require(R2jags)
 require(ggplot2)
-source("..//Auxiliar_functions/jagsresults.R")
-
+source("https://raw.githubusercontent.com/johnbaums/jagstools/master/R/jagsresults.R")
+uv <-read.csv("logit_dataset.csv")
 
 set.seed(1056)                           # set seed to replicate example
-nobs= 1000                               # number of obsservations
-x1 <- runif(nobs,0,3)                    # random uniform variable
-eta <- -6 + 3.5*x1                       # linear predictor
-p <- 1/(1+exp(-eta))                     # inverse-logit link
-by <- rbinom(nobs,size=1, prob = p)      # create y as adjusted random bernoulli variate
+nobs= nrow(uv)                             # number of obsservations
+x1 <- uv$redshift                    # random uniform variable
+by <- uv$logit_class.1.uvup.2.uvweak.      # create y as adjusted random bernoulli variate
 
 
 # Prepare data for prediction
@@ -32,7 +30,7 @@ xx = seq(from =  min(x1),
 logitmod <-data.frame(by, x1)
 X <- model.matrix(~ 1+x1,
                   data = logitmod)
-K <- ncol(X)
+K <- 3
 logit_data <- list(Y  = logitmod$by, # Response variable
                    X  = X,           # Predictors
                    K  = K,           # Number of Predictors including the intercept
@@ -51,12 +49,12 @@ for (i in 1:K) { beta[i] ~ dnorm(0, 0.0001) }
 for (i in 1:N){
 Y[i] ~ dbern(p[i])
 logit(p[i]) <- eta[i]
-eta[i]  <- beta[1]+beta[2]*X[i,2]
+eta[i]  <- beta[1]+beta[2]*X[i,2]+beta[3]*X[i,2]^2
 }
 
 # Prediction for new data
 for (j in 1:M){
-etax[j]<-beta[1]+beta[2]*xx[j]
+etax[j]<-beta[1]+beta[2]*xx[j]+beta[3]*xx[j]^2
 logit(px[j]) <- etax[j]
 Yx[j]~dbern(px[j])
 }
@@ -64,7 +62,7 @@ Yx[j]~dbern(px[j])
 
 #A function to generate initial values for mcmc
 inits  <- function () {
-  list(beta  = rnorm(ncol(X), 0, 0.1)  )
+  list(beta  = rnorm(3, 0, 0.1)  )
 }
 
 # define parameters
@@ -91,22 +89,22 @@ x <- xx
 gdata <- data.frame(x =xx, mean = y[,"mean"],lwr1=y[,"25%"],lwr2=y[,"2.5%"],upr1=y[,"75%"],upr2=y[,"97.5%"])
 
 # Bin data for visualization
-binx<-0.25
-t.breaks <-cut(x1, seq(0,3, by=binx))
+binx<-0.1
+t.breaks <-cut(x1, seq(min(x1),max(x1), by=binx))
 means <-tapply(by, t.breaks, mean)
 semean <-function(x) sd(x)/sqrt(length(x))
 means.se <-tapply(by, t.breaks, semean)
 
 
-gbin<-data.frame(x=seq(binx,3, by=binx),y=means)
+gbin<-data.frame(x=seq(binx,max(x1), by=binx),y=means)
 
 ggplot(logitmod,aes(x=x1,y=by))+
-  geom_point(colour="red",size=1,alpha=0.85,position = position_jitter (h = 0.075))+
-  geom_point(aes(x=x,y=y),size=2.75,data=gbin,colour="blue3")+
+  geom_point(colour="#dd0100",size=1,alpha=0.45,position = position_jitter (h = 0.025))+
+ geom_ribbon(data=gdata,aes(x=xx,ymin=lwr2, ymax=upr2,y=NULL), alpha=0.95, fill = c("#fac901"),show.legend=FALSE) +
+  geom_ribbon(data=gdata,aes(x=xx,ymin=lwr1, ymax=upr1,y=NULL), alpha=0.95, fill=c("#225095"),show.legend=FALSE) +
+  geom_point(aes(x=x,y=y),size=2.75,data=gbin,colour="#dd0100")+
   geom_errorbar(data=gbin,aes(x=x,y=y,ymin=y-2*means.se,ymax=y+2*means.se),alpha=0.85,
-                colour="blue",width=0.005)+
-  geom_ribbon(data=gdata,aes(x=xx,ymin=lwr1, ymax=upr1,y=NULL), alpha=0.45, fill=c("orange2"),show.legend=FALSE) +
-  geom_ribbon(data=gdata,aes(x=xx,ymin=lwr2, ymax=upr2,y=NULL), alpha=0.35, fill = c("orange"),show.legend=FALSE) +
-  geom_line(data=gdata,aes(x=xx,y=mean),colour="gray25",linetype="dashed",size=1,show.legend=FALSE)+
-  theme_bw()
+                colour="#dd0100",width=0.005)+
+  geom_line(data=gdata,aes(x=xx,y=mean),colour="#ffffff",linetype="dashed",size=1,show.legend=FALSE)+
+  theme_bw() + xlab("Redshift") + ylab("UV/Red-sequence")
 
